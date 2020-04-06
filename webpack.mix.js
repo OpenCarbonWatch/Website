f511681fff672ce0311c0132ba3411f2217c3362
Open Carbon Watch Admin <57277151+OpenCarbonWatchAdmin@users.noreply.github.com>
2020-04-06 14:27:27 +0200

    Drop support for Internet Explorer
    This makes the search GUI impossible to use for 1% of users but allows us to spare ~ 40 kb/gzip for 100% of users.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-require('laravel-mix-polyfill');
 require('laravel-mix-purgecss');
 
 /*
@@ -16,10 +15,5 @@ require('laravel-mix-purgecss');
 mix
     .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .polyfill({
-        enabled: true,
-        useBuiltIns: "usage",
-        targets: {"firefox": "50", "ie": 11}
-    })
     .purgeCss()
     .version();
