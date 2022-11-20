ea436cdbf6a740dc892cce642da319eaf9575605
Open Carbon Watch Admin <57277151+OpenCarbonWatchAdmin@users.noreply.github.com>
2022-11-20 10:30:13 +0100

    Upgrade dependencies to latest versions

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,27 @@
 const mix = require('laravel-mix');
-require('laravel-mix-purgecss');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix
     .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .purgeCss()
     .version()
     .vue();
+
+// In production, purge CSS of unnecessary classes by filtering only those used in our JS / Vue / Blade views.
+// We use the PurgeCSS plugin for PostCSS, without the Laravel wrappers, which seem deprecated as on 2022-11-20.
+if (mix.inProduction()) {
+    mix.options({
+        postCss: [
+            require("@fullhuman/postcss-purgecss")({
+                content: [
+                    "resources/js/**/*.js",
+                    "resources/js/**/*.vue",
+                    "resources/views/**/*.php",
+                ],
+                defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+                safelist: {
+                    greedy: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+                },
+            }),
+        ],
+    });
+}
